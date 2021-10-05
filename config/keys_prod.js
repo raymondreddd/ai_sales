c6147212f10c5a8d55283dc023e2f33cd0fe42a2
Kudu <raymondreddington40@gmail.com>
2021-10-05 22:45:33 +0530

    Was not paying attention to keys_prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
     stripePublishableKey: 'process.env.STRIPE_PUBLISHABLE_KEY',
-    stripeSecretKey: 'STRIPE_SECRET_KEY',
+    stripeSecretKey: 'process.env.STRIPE_SECRET_KEY',
     your_domain:'https://guarded-headland-72288.herokuapp'
 }
