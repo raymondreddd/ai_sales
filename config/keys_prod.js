8bc67adcd5975ccb6452342014b85cbcba7fe62c
Kudu <raymondreddington40@gmail.com>
2021-10-05 22:49:28 +0530

    Very smart mistake passing process.env.scretkey as string

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    stripePublishableKey: 'process.env.STRIPE_PUBLISHABLE_KEY',
-    stripeSecretKey: 'process.env.STRIPE_SECRET_KEY',
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
     your_domain:'https://guarded-headland-72288.herokuapp'
 }
